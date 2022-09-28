00a2eaab550ecbb34283832d25115cf6a3dd5bcc
MeshaS32 <kamesha.hamilton@codeimmersives.com>
2022-09-28 15:51:01 -0400

    assisgnment

--- lots-of-functions.js
+++ lots-of-functions.js
@@ -10,40 +10,43 @@ interpolation
 
 */
 
-// // function sayHi(name){
-// //     let newStr = `Hi, ${name}`;
-// //     return newStr
-// // }
-// // console.log(sayHi("Kamesha"));
-// // console.log(sayHi("Jimmy"))
-// // console.log(sayHi("Adira"))
+//#1
+ function sayHi(name){
+    let newStr = `Hi, ${name}`;
+     return newStr
+ }
+ console.log(sayHi("Kamesha"));
+ console.log(sayHi("Jimmy"))
+console.log(sayHi("Adira"))
 
-// function bigString(str1, str2){
-//     if(str1.length > str2.length){
-//       return str1
-//     } else if( str2.length > str1.length){
-//     return str2
-// } else if(str1.length === str2.length){
-//     console.log(`${str1} is equal in length to ${str2}`)
-// }
-// }
+//#2
+function bigString(str1, str2){
+    if(str1.length > str2.length){
+      return str1
+    } else if( str2.length > str1.length){
+    return str2
+} else if(str1.length === str2.length){
+    console.log(`${str1} is equal in length to ${str2}`)
+}
+}
 
-// console.log(bigString("blue", "red"))
-// console.log(bigString("twin", "red"))
-// bigString("Two", "red")
+console.log(bigString("blue", "red"))
+console.log(bigString("twin", "red"))
+bigString("Two", "red")
 
-// function bigNumber(num1, num2){
-//     if(num1 > num2){
-//         return num1
-//     }else if(num2 > num1){
-//         return num2
-//     }else if(num1 === num2){
-//         console.log(`${num1} is equal to ${num2}`)
-//     }
-// }
-// console.log(bigNumber(200,100))
-// console.log(bigNumber(100, 200))
-// bigNumber(100,100)
+//#3
+function bigNumber(num1, num2){
+    if(num1 > num2){
+        return num1
+    }else if(num2 > num1){
+        return num2
+    }else if(num1 === num2){
+        console.log(`${num1} is equal to ${num2}`)
+    }
+}
+console.log(bigNumber(200,100))
+console.log(bigNumber(100, 200))
+bigNumber(100,100)
 
 /* function name: fiveMore
 parameter- array of numbers
@@ -55,34 +58,34 @@ myArray.length
 
 
 //#4
-// function fiveMore(myArray){
+function fiveMore(myArray){
 
-//     for(let i = 0; i < myArray.length; i++){
+    for(let i = 0; i < myArray.length; i++){
 
-//     if(myArray[i] % 2 !== 0){
-//         myArray[i] += 5
-//     }
-// }
-//     return myArray;
-// }
-// console.log(fiveMore([1,2,3,4,]))
+    if(myArray[i] % 2 !== 0){
+        myArray[i] += 5
+    }
+}
+    return myArray;
+}
+console.log(fiveMore([1,2,3,4,]))
 
 //#5
 
-// function arraySummer(myArray2){
-//     let sum = 0
+function arraySummer(myArray2){
+    let sum = 0
 
-//     for( let i2 = 0; i2 < myArray2.length; i2++){
+    for( let i2 = 0; i2 < myArray2.length; i2++){
 
-//            sum += myArray2[i2]
-//         }
+           sum += myArray2[i2]
+        }
     
-//     return sum
-// }
-// console.log(arraySummer([1,2,3,4,6]))
+    return sum
+}
+console.log(arraySummer([1,2,3,4,6]))
 
 
-/*#6
+/*
 
 function name: everyDivisible
 parameter: n - number les than 10
@@ -93,6 +96,7 @@ if( number % n === 0)
 print/ console.log(number)
 
 */
+//#6
 
 function everyDivisible(n){
     
